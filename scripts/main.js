619ca14cb687daf52e278149498f73f86a969871
Antyneutrino <antyneutrino@github.com>
2020-01-09 10:33:26 +0100

    Adding-files

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function showTitle() {
+    var myHeading = document.querySelector('h1');
+    myHeading.textContent = 'Marvel'; 
+}
+function showMean() {
+    var myHeading = document.querySelector('h1');
+    myHeading.textContent = 'Universe'; 
+}
